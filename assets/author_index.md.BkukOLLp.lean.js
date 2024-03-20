c8bfb6b83240aed6ffcfe21bcb1900befc4fc5bf
honglm <honglm@fits.com.cn>
2024-03-20 16:27:48 +0800

    deploy

---
+++ assets/author_index.md.BkukOLLp.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as t,o as e,a3 as r}from"./chunks/framework.CGTGJaqa.js";const m=JSON.parse('{"title":"关于我们","description":"","frontmatter":{},"headers":[],"relativePath":"author/index.md","filePath":"author/index.md"}'),o={name:"author/index.md"},n=r("",10),i=[n];function s(l,c,h,d,_,p){return e(),t("div",null,i)}const u=a(o,[["render",s]]);export{m as __pageData,u as default};
